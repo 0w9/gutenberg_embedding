64135ed58d066fb935b5b8abf3e5f679c0580ead
Lennard Dorst <lennardsolana@gmail.com>
2024-06-10 10:14:41 +0200

    adding existing scraper

---
+++ gutenberg/items.py
@@ -0,0 +1,23 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class GutenbergItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
+class GutenbergEbook(scrapy.Item):
+    index = scrapy.Field()
+    id = scrapy.Field()
+    title = scrapy.Field()
+    author = scrapy.Field()
+    downloads = scrapy.Field()
+    text = scrapy.Field()
+    text_file = scrapy.Field()
+    
+    pass
---
+++ gutenberg/spiders/project_gutenberg.py
@@ -0,0 +1,36 @@
+import scrapy, requests
+
+from gutenberg.items import GutenbergEbook
+
+class GutenbergSpider(scrapy.Spider):
+    name = "project_gutenberg"
+    allowed_domains = ["www.gutenberg.org"]
+    start_urls = ["https://www.gutenberg.org/ebooks/bookshelf/57"]
+    index = 0
+
+    def parse(self, response):
+        results = response.css("ul.results")
+        
+        if not results:
+            return
+        
+        books = results.css("li.booklink")
+        
+        for book in books:
+            content = book.css("span.content")
+            link = book.css("a.link::attr(href)").get()
+            id = link.split("/ebooks/")[1]
+            text = requests.get(f"https://www.gutenberg.org/ebooks/{id}.txt.utf-8").text
+            
+            ebook = GutenbergEbook()
+            ebook["index"] = self.index
+            ebook["id"] = id
+            ebook["title"] = content.css("span.title::text").get()
+            ebook["author"] = content.css("span.subtitle::text").get()
+            ebook["downloads"] = content.css("span.extra::text").get()
+            ebook["text"] = text
+            ebook["text_file"] = f"https://www.gutenberg.org/ebooks/{id}.txt.utf-8"
+            
+            self.index += 1
+            
+        yield scrapy.Request(f"https://www.gutenberg.org/ebooks/bookshelf/57?start_index={self.index}", callback=self.parse) 
